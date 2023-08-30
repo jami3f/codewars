1aaf9a091dec541b2197748512198dbc98785fb7
Jamie Fairhurst <jamie.fairhurst01@gmail.com>
2023-08-30 16:39:20 +0100

    Setup camel case project

---
+++ rust/camelcase/src/main.rs
@@ -0,0 +1,24 @@
+
+fn to_camel_case(text: &str) -> String {}
+
+// Add your tests here.
+// See https://doc.rust-lang.org/stable/rust-by-example/testing/unit_testing.html
+
+#[cfg(test)]
+mod tests {
+    use super::to_camel_case;
+
+    const ERR_MSG: &str = "\nYour result (left) did not match the expected output (right)";
+
+    fn dotest(s: &str, expected: &str) {
+        assert_eq!(to_camel_case(s), expected, "{ERR_MSG} with text = \"{s}\"")
+    }
+
+    #[test]
+    fn fixed_tests() {
+        dotest("", "");
+        dotest("the_stealth_warrior", "theStealthWarrior");
+        dotest("The-Stealth-Warrior", "TheStealthWarrior");
+        dotest("A-B-C", "ABC");
+    }
+}
---
+++ rust/camelcase/target/debug/deps/camelcase-8cee8d05598648de.d
@@ -0,0 +1,5 @@
+/home/jamie/github/codewars/rust/camelcase/target/debug/deps/camelcase-8cee8d05598648de: src/main.rs
+
+/home/jamie/github/codewars/rust/camelcase/target/debug/deps/camelcase-8cee8d05598648de.d: src/main.rs
+
+src/main.rs:
